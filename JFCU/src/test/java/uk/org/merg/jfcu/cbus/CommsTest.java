c97fc7247262bfeed90b6de90d2c8f271cbaf750
Ian Hogg <ian.w.hogg@virginmedia.com>
2019-12-05 09:27:05 +0000

    Changed src folder structure to standard Mavern form. Added src/test/java folder and a couple of auto-generated Junit tests

---
+++ JFCU/src/test/java/uk/org/merg/jfcu/cbus/CommsTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package uk.org.merg.jfcu.cbus;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Ian
+ *
+ */
+public class CommsTest {
+
+	/**
+	 * Test method for {@link uk.org.merg.jfcu.cbus.Comms#prepareCommsDriver(java.lang.String[])}.
+	 */
+	@Test
+	public void testPrepareCommsDriver() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link uk.org.merg.jfcu.cbus.Comms#connect(java.lang.String)}.
+	 */
+	@Test
+	public void testConnect() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link uk.org.merg.jfcu.cbus.Comms#close()}.
+	 */
+	@Test
+	public void testClose() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ JFCU/src/test/java/uk/org/merg/jfcu/cbus/GlobalsTest.java
@@ -0,0 +1,25 @@
+package uk.org.merg.jfcu.cbus;
+/**
+ * 
+ */
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Ian
+ *
+ */
+public class GlobalsTest {
+
+	/**
+	 * Test method for {@link uk.org.merg.jfcu.cbus.Globals#init(javax.swing.text.StyledDocument)}.
+	 */
+	@Test
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+}
