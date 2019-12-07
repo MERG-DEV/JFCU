87ad2fe0fbcc3055d9c46c84af6072eeda666b1a
david <david@ellis128.co.uk>
2019-12-07 22:59:32 +0000

    Added ByteOps unit test

---
+++ JFCU/src/test/java/uk/org/merg/jfcu/events/Test_ByteOps.java
@@ -0,0 +1,93 @@
+package uk.org.merg.jfcu.events;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class Test_ByteOps {
+
+	@ParameterizedTest
+	@ValueSource(ints = {-127, -1, 8, 128})
+	void test_getBitException(int number) {
+		ByteOp byteOp = new ByteOp();
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			  byteOp.getBit(number);
+			  });
+	}
+	
+	@ParameterizedTest
+	@ValueSource(ints = {0,7})
+	void test_Clear(int number) {
+		ByteOp byteOp = new ByteOp();
+		byteOp.setBit(number, BitOp.CLEAR);
+		assertEquals(BitOp.CLEAR, byteOp.getBit(number));
+	}
+	
+	@ParameterizedTest
+	@ValueSource(ints = {-127, -1, 8, 128})
+	void test_ClearException(int number) {
+		ByteOp byteOp = new ByteOp();
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			  byteOp.setBit(number, BitOp.CLEAR);
+			  });
+	}
+
+
+	@ParameterizedTest
+	@ValueSource(ints = {0,7})
+	void test_Set(int number) {
+		ByteOp byteOp = new ByteOp();
+		byteOp.setBit(number, BitOp.SET);
+		assertEquals(BitOp.SET, byteOp.getBit(number));
+	}
+	
+	@ParameterizedTest
+	@ValueSource(ints = {-127, -1, 8, 128})
+	void test_SetException(int number) {
+		ByteOp byteOp = new ByteOp();
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			  byteOp.setBit(number, BitOp.SET);
+			  });
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {0,7})
+	void test_Unchanged(int number) {
+		ByteOp byteOp = new ByteOp();
+		byteOp.setBit(number, BitOp.UNCHANGED);
+		assertEquals(BitOp.UNCHANGED, byteOp.getBit(number));
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {-127, -1, 8, 128})
+	void test_UnchangedException(int number) {
+		ByteOp byteOp = new ByteOp();
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			  byteOp.setBit(number, BitOp.UNCHANGED);
+			  });
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {0,7})
+	void test_asIntSet(int number) {
+		ByteOp byteOp = new ByteOp();
+		byteOp.setBit(number, BitOp.SET);
+		assertEquals(1<<number, byteOp.asInt());
+	}
+	
+	@ParameterizedTest
+	@ValueSource(ints = {0,7})
+	void test_asIntClear(int number) {
+		ByteOp byteOp = new ByteOp();
+		byteOp.setBit(number, BitOp.SET);
+		assertEquals(1<<number, byteOp.asInt());
+		byteOp.setBit(number, BitOp.UNCHANGED);
+		assertEquals(0, byteOp.asInt());
+		byteOp.setBit(number, BitOp.CLEAR);
+		assertEquals(0, byteOp.asInt());
+	}
+
+}
