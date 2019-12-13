3557d5baa82e3b6b91dfd51662dc6963c15cc96c
david elllis <david@ellis128.co.uk>
2019-12-13 19:53:56 +0000

    Minimised changes to JFCUfx.java to just those necessary to get TestFX to work

--- JFCU/src/main/java/uk/org/merg/jfcu/ui/javafx/JFCUfx.java
+++ JFCU/src/main/java/uk/org/merg/jfcu/ui/javafx/JFCUfx.java
@@ -57,216 +57,256 @@ public class JFCUfx extends Application {
             stage.setHeight(600);
             stage.setWidth(800);
             
-          MenuBar menuBar = new MenuBar();
-          menuBar.setId("menuBar");
+//            URL url = JFCUfx.class.getResource(
+//                    "/css/jfcu.css");
+//            scene.getStylesheets().add(url.toString());
 
-  		  Menu menuFile = new Menu("File");
-  		  menuFile.setId("fileMenu");
+            MenuBar menuBar = new MenuBar();
+            menuBar.setId("menuBar");
 
-  		  
-		  MenuItem mi = new MenuItem("Open...");
-	        mi.setOnAction(new EventHandler<ActionEvent>() {
-				@Override
-				public void handle(ActionEvent arg0) {
-					 FileChooser fileChooser = new FileChooser();
-					 fileChooser.setTitle("Open Layout File");
-					 fileChooser.getExtensionFilters().addAll(
-					         new ExtensionFilter("XML Files", "*.xml"),
-					         new ExtensionFilter("All Files", "*.*"));
-					 File selectedFile = fileChooser.showOpenDialog(stage);
-					 if (selectedFile != null) {
-					    Globals.layout = Layout.load(selectedFile.getAbsolutePath());
-					 }
-				}});
-		  menuFile.getItems().add(mi);
-		  
-  		  mi = new MenuItem("Save...");
-          mi.setOnAction(new EventHandler<ActionEvent>() {
-  			@Override
-  			public void handle(ActionEvent arg0) {
-  				FileChooser fileChooser = new FileChooser();
-  				 fileChooser.setTitle("Save Layout File");
-  				 fileChooser.getExtensionFilters().addAll(
-  				         new ExtensionFilter("XML Files", "*.xml"),
-  				         new ExtensionFilter("All Files", "*.*"));
-  				 File selectedFile = fileChooser.showOpenDialog(stage);
-  				 if (selectedFile != null) {
-  				    Globals.layout.save(selectedFile.getAbsolutePath());
-  				 }
-  			}});
-		  menuFile.getItems().add(mi);
-		  
-		  mi = new MenuItem("Load from CBUS"); 
-	        mi.setOnAction(new EventHandler<ActionEvent>() {
-				@Override
-				public void handle(ActionEvent arg0) {
-					Globals.layout.getModules().clear();
-					Globals.layout.getModulesProperty().clear();
-					Globals.layout.getEvents().clear();
-					Globals.layout.eventsProperty().clear();
-					Globals.layout.loadFromCBUS();
-				}});
-		  menuFile.getItems().add(mi);
-		  
-		  mi = new MenuItem("Exit"); 
-	        mi.setOnAction(new EventHandler<ActionEvent>() {
-				@Override
-				public void handle(ActionEvent arg0) {
-					Platform.exit();
-				}});
-		  menuFile.getItems().add(mi);
-		  
-		  Menu menuEvents = new Menu("Events"); Menu menuNodes = new Menu("Nodes");
-		  Menu menuCommunications = new Menu("Communications");
-		  
-			
-			  ToggleGroup connectGroup = new ToggleGroup(); String[] portList; portList =
-			  Comms.theDriver.getPortNames(); if (portList.length > 0) portText=null;
-			  System.out.println("Portlist len="+portList.length); for (String p :
-			  portList) { RadioMenuItem item = new RadioMenuItem(p); item.setOnAction(new
-			  EventHandler<ActionEvent>() {
-			  
-			  @Override public void handle(ActionEvent e) {
-			  System.out.println("radio 1 toggled e="+e); Comms.connect(item.getText()); }
-			  }); connectGroup.getToggles().add(item);
-			  menuCommunications.getItems().add(item); } if ((portText != null) &&
-			  (portText.length() > 0)) { RadioMenuItem item = new RadioMenuItem(portText);
-			  item.setOnAction(new EventHandler<ActionEvent>() {
-			  
-			  @Override public void handle(ActionEvent e) {
-			  System.out.println("radio 2 toggled e="+e); Comms.connect(item.getText()); }
-			  }); connectGroup.getToggles().add(item);
-			  menuCommunications.getItems().add(item); } mi = new MenuItem("Define Port");
-			  mi.setOnAction(new EventHandler<ActionEvent>() {
-			  
-			  @Override public void handle(ActionEvent arg0) { TextInputDialog dialog = new
-			  TextInputDialog(); dialog.setTitle("Define Port");
-			  dialog.setHeaderText("Enter port device name"); Optional<String> result =
-			  dialog.showAndWait(); if (result.isPresent()) { portText = result.get();
-			  RadioMenuItem item = new RadioMenuItem(portText); item.setOnAction(new
-			  EventHandler<ActionEvent>() {
-			  
-			  @Override public void handle(ActionEvent e) {
-			  System.out.println("radio 3 toggled e="+e); Comms.connect(item.getText()); }
-			  }); connectGroup.getToggles().add(item);
-			  menuCommunications.getItems().add(item); } } });
-			  menuCommunications.getItems().add(mi);
-			 		  
-		  
-		  Menu menuSettings = new Menu("Settings");
-		  
-		  Menu menuLogging = new Menu("Logging"); 
-		  
-		  Menu menuHelp = new Menu("Help"); 
-		  
-		  mi = new MenuItem("Version");
-		  mi.setOnAction(new EventHandler<ActionEvent>() {
-			  @Override public void handle(ActionEvent arg0) { 
-				  Alert alert = new Alert(AlertType.INFORMATION); 
-				  alert.setContentText(VERSION);
-				  alert.showAndWait(); 
-				  }
-			  }
-		  );
-		  menuHelp.getItems().add(mi); 
+    		  Menu menuFile = new Menu("File");
+    		  menuFile.setId("fileMenu");
+            
+            MenuItem mi = new MenuItem("Open...");
+            mi.setOnAction(new EventHandler<ActionEvent>() {
+    			@Override
+    			public void handle(ActionEvent arg0) {
+    				 FileChooser fileChooser = new FileChooser();
+    				 fileChooser.setTitle("Open Layout File");
+    				 fileChooser.getExtensionFilters().addAll(
+    				         new ExtensionFilter("XML Files", "*.xml"),
+    				         new ExtensionFilter("All Files", "*.*"));
+    				 File selectedFile = fileChooser.showOpenDialog(stage);
+    				 if (selectedFile != null) {
+    				    Globals.layout = Layout.load(selectedFile.getAbsolutePath());
+    				 }
+    			}});
+            menuFile.getItems().add(mi);
+            
+            mi = new MenuItem("Save...");
+            mi.setOnAction(new EventHandler<ActionEvent>() {
+    			@Override
+    			public void handle(ActionEvent arg0) {
+    				FileChooser fileChooser = new FileChooser();
+    				 fileChooser.setTitle("Save Layout File");
+    				 fileChooser.getExtensionFilters().addAll(
+    				         new ExtensionFilter("XML Files", "*.xml"),
+    				         new ExtensionFilter("All Files", "*.*"));
+    				 File selectedFile = fileChooser.showOpenDialog(stage);
+    				 if (selectedFile != null) {
+    				    Globals.layout.save(selectedFile.getAbsolutePath());
+    				 }
+    			}});
+            menuFile.getItems().add(mi);
+            
+            mi = new MenuItem("Load from CBUS");
+            mi.setOnAction(new EventHandler<ActionEvent>() {
+    			@Override
+    			public void handle(ActionEvent arg0) {
+    				Globals.layout.getModules().clear();
+    				Globals.layout.getModulesProperty().clear();
+    				Globals.layout.getEvents().clear();
+    				Globals.layout.eventsProperty().clear();
+    				Globals.layout.loadFromCBUS();
+    			}});
+            menuFile.getItems().add(mi);
+            
+            mi = new MenuItem("Exit");
+            mi.setOnAction(new EventHandler<ActionEvent>() {
+    			@Override
+    			public void handle(ActionEvent arg0) {
+    				Platform.exit();
+    			}});
+            menuFile.getItems().add(mi);
+            
+            Menu menuEvents = new Menu("Events");
+            Menu menuNodes = new Menu("Nodes");
+            Menu menuCommunications = new Menu("Communications");
+            
+            ToggleGroup connectGroup = new ToggleGroup();
+            String[] portList;
+            portList = Comms.theDriver.getPortNames();
+            if (portList.length > 0) portText=null;
+            System.out.println("Portlist len="+portList.length);
+            for (String p : portList) {
+            	RadioMenuItem item = new RadioMenuItem(p);
+            	item.setOnAction(new EventHandler<ActionEvent>() {
+    			    @Override public void handle(ActionEvent e) {
+    			        System.out.println("radio 1 toggled e="+e);
+    			        Comms.connect(item.getText());
+    			    }
+    			});
+            	connectGroup.getToggles().add(item);
+            	menuCommunications.getItems().add(item);
+            }
+            if ((portText != null) && (portText.length() > 0)) {
+            	RadioMenuItem item = new RadioMenuItem(portText);
+            	item.setOnAction(new EventHandler<ActionEvent>() {
+    			    @Override public void handle(ActionEvent e) {
+    			        System.out.println("radio 2 toggled e="+e);
+    			        Comms.connect(item.getText());
+    			    }
+    			});
+            	connectGroup.getToggles().add(item);
+            	menuCommunications.getItems().add(item);
+            }
+    		  mi = new MenuItem("Define Port");
+    	      mi.setOnAction(new EventHandler<ActionEvent>() {
+    				@Override
+    				public void handle(ActionEvent arg0) {
+    					TextInputDialog dialog = new TextInputDialog();
+    					dialog.setTitle("Define Port");
+    					dialog.setHeaderText("Enter port device name");
+    					Optional<String> result = dialog.showAndWait();
+    					if (result.isPresent()) {
+    						portText = result.get();
+    					    RadioMenuItem item = new RadioMenuItem(portText);
+    	        			item.setOnAction(new EventHandler<ActionEvent>() {
+    	        			    @Override public void handle(ActionEvent e) {
+    	        			        System.out.println("radio 3 toggled e="+e);
+    	        			        Comms.connect(item.getText());
+    	        			    }
+    	        			});
+    	                	connectGroup.getToggles().add(item);
+    	                	menuCommunications.getItems().add(item);
+    	                }
+    	        	}
+    	      });
+    	      menuCommunications.getItems().add(mi);
+            
+            
+            
+            Menu menuSettings = new Menu("Settings");
 
-		  mi = new MenuItem("Dump Layout"); 
-		  mi.setOnAction(new EventHandler<ActionEvent>() {
-			  @Override public void handle(ActionEvent arg0) {
-				  System.out.println(Globals.layout); 
-				  }
-			  }
-		  );
-		  menuHelp.getItems().add(mi);
-		  
-		  mi = new MenuItem("Update Version"); 
-		  menuHelp.getItems().add(mi);
-		  
-		  menuBar.getMenus().addAll(menuFile, menuEvents, menuNodes,
-		  menuCommunications, menuSettings, menuLogging, menuHelp);
+            
+            Menu menuLogging = new Menu("Logging");
+            Menu menuHelp = new Menu("Help");
+            mi = new MenuItem("Version");
+            mi.setOnAction(new EventHandler<ActionEvent>() {
+    			@Override
+    			public void handle(ActionEvent arg0) {
+    				Alert alert = new Alert(AlertType.INFORMATION);
+    				alert.setContentText(VERSION);
+    				alert.showAndWait();
+    			}});
+            menuHelp.getItems().add(mi);
+            mi = new MenuItem("Dump Layout");
+            mi.setOnAction(new EventHandler<ActionEvent>() {
+    			@Override
+    			public void handle(ActionEvent arg0) {
+    				System.out.println(Globals.layout);
+    			}});
+            menuHelp.getItems().add(mi);
+     /*       mi = new MenuItem("Update Version");
+            mi.setOnAction(new EventHandler<ActionEvent>() {
+    			@Override
+    			public void handle(ActionEvent arg0) {
+    				Module m = Globals.layout.findModule(1114);
+    				m.getParams().put(CbusProperties.MINOR_VERSION.getValue(), (int)'z');
+    				m.getParams().put(CbusProperties.NUM_NV.getValue(), 5555);
+    				m.setNodeNumber(666);
+    			}});
+            menuHelp.getItems().add(mi); */
+            menuBar.getMenus().addAll(menuFile, menuEvents, menuNodes, menuCommunications, menuSettings,
+            		menuLogging, menuHelp);
+     
+            HBox hbox = new HBox();
+            
+            // Node list
+        	NodeListTable nodeList = new NodeListTable();
+            
+            // Event list
+            EventListTable eventList = new EventListTable();
 
-		  
-		   HBox hbox = new HBox();
-		  
-		    // Node list 
-			NodeListTable nodeList = new NodeListTable();
-			// Event list 
-			EventListTable eventList = new EventListTable();
-
-			//Log
-	        TextArea log = new TextArea("> QNN\n< PNN NN=1111, Manu=MERG, Module=CANMIO, Flags=FLiM&BOOT\n\n\n\n\n\n\n\n\n\n");
-
-	        Thread logUpdater = new Thread() {
-	        	@Override
-				public void run(){
-	        		while (true) {
-	        			try {
-							log.setText(Globals.log.getText(0, Globals.log.getLength()));
-						} catch (BadLocationException e1) {
-							e1.printStackTrace();
-						}
-	        			try {
-							sleep(50);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-	        		}
-	        	}
-	        };
-	        logUpdater.start();
-	        log.getStyleClass().add("cbus-log");
-	        log.setPrefRowCount(1000);
-	        
-	    VBox lhs = new VBox();
-
-
-	    Label lhsTitle = new Label("User CBUS Nodes");
-        lhsTitle.getStyleClass().add("lhs-title");
-        lhs.getChildren().addAll(lhsTitle, nodeList);
-        lhsTitle = new Label("Events associated with Node");
-        lhsTitle.getStyleClass().add("lhs-title");
-        lhs.getChildren().addAll(lhsTitle, eventList);
-        hbox.getChildren().addAll(lhs);  
-	        
-        VBox rhs = new VBox();
-
-        HBox rhsTitle = new HBox();
-        Label rhsLabel = new Label("CBUS log");
-        rhsLabel.getStyleClass().add("rhs-title");
-        rhsTitle.getChildren().add(rhsLabel);
-        Button button = new Button("Clear");
-        
-        button.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				try {
-					Globals.log.remove(0,Globals.log.getLength());
-				} catch (BadLocationException e) {
-					e.printStackTrace();
-				}
-			}});
-		
-        rhsTitle.getChildren().add(button);
-        rhs.getChildren().addAll(rhsTitle, log);
-        hbox.getChildren().addAll(rhs);  
-
-        HBox bottomBox = new HBox();
-
-        String driverName = Comms.theDriver.getClass().toString();
-        String [] driverNameParts = driverName.split("\\.");
-        Label bottomLabel = new Label("Driver:"+driverNameParts[driverNameParts.length-1]);
-        bottomLabel.getStyleClass().add("bottomLabel");
-        bottomBox.getChildren().add(bottomLabel);
-        HBox portLabelBox = new HBox();
-        portLabelBox.getStyleClass().add("bottomLabel");
-        bottomLabel = new Label("COM Port:");
-        portLabelBox.getChildren().add(bottomLabel);
-        bottomLabel = new Label(Comms.portName.get());
-        bottomLabel.textProperty().bind(Comms.portName);
-        portLabelBox.getChildren().add(bottomLabel);
-        bottomBox.getChildren().add(portLabelBox);
-        bottomBox.setId("bottomBox");
-		  
+            //Log
+            TextArea log = new TextArea("> QNN\n< PNN NN=1111, Manu=MERG, Module=CANMIO, Flags=FLiM&BOOT\n\n\n\n\n\n\n\n\n\n");
+            Thread logUpdater = new Thread() {
+            	@Override
+    			public void run(){
+            		while (true) {
+            			try {
+    						log.setText(Globals.log.getText(0, Globals.log.getLength()));
+    					} catch (BadLocationException e1) {
+    						e1.printStackTrace();
+    					}
+            			try {
+    						sleep(50);
+    					} catch (InterruptedException e) {
+    						e.printStackTrace();
+    					}
+            		}
+            	}
+            };
+            logUpdater.start();
+            log.getStyleClass().add("cbus-log");
+            log.setPrefRowCount(1000);
+            //Region region = ( Region ) log.lookup( ".content" );
+            //System.out.println("region="+region);
+            //region.setStyle( "-fx-background-color: yellow" );
+            VBox lhs = new VBox();
+            Label lhsTitle = new Label("User CBUS Nodes");
+            lhsTitle.getStyleClass().add("lhs-title");
+            lhs.getChildren().addAll(lhsTitle, nodeList);
+            lhsTitle = new Label("Events associated with Node");
+            lhsTitle.getStyleClass().add("lhs-title");
+            lhs.getChildren().addAll(lhsTitle, eventList);
+            
+            VBox rhs = new VBox();
+            HBox rhsTitle = new HBox();
+            Label rhsLabel = new Label("CBUS log");
+            rhsLabel.getStyleClass().add("rhs-title");
+            rhsTitle.getChildren().add(rhsLabel);
+            Button button = new Button("Clear");
+            
+            button.setOnAction(new EventHandler<ActionEvent>() {
+    			@Override
+    			public void handle(ActionEvent arg0) {
+    				try {
+    					Globals.log.remove(0,Globals.log.getLength());
+    				} catch (BadLocationException e) {
+    					e.printStackTrace();
+    				}
+    			}});
+            rhsTitle.getChildren().add(button);
+            
+    /*        // test code
+            button = new Button("Test");
+            button.setOnAction(new EventHandler<ActionEvent>() {
+    			@Override
+    			public void handle(ActionEvent arg0) {
+    				Module m = Globals.layout.findModule(1111);
+    				if (m == null) {
+    					System.out.println("not found");
+    					return;
+    				}
+    				int nnv = m.getNumNV();
+    				System.out.println("got nnv="+nnv);
+    				m.setNumNV(++nnv);
+    				System.out.println("put nnv="+nnv);
+    			}});
+            rhsTitle.getChildren().add(button);
+            //end text code/* */
+            
+            
+            rhs.getChildren().addAll(rhsTitle, log);
+            hbox.getChildren().addAll(lhs, rhs);  
+            
+            HBox bottomBox = new HBox();
+            String driverName = Comms.theDriver.getClass().toString();
+            String [] driverNameParts = driverName.split("\\.");
+            Label bottomLabel = new Label("Driver:"+driverNameParts[driverNameParts.length-1]);
+            bottomLabel.getStyleClass().add("bottomLabel");
+            bottomBox.getChildren().add(bottomLabel);
+            HBox portLabelBox = new HBox();
+            portLabelBox.getStyleClass().add("bottomLabel");
+            bottomLabel = new Label("COM Port:");
+            portLabelBox.getChildren().add(bottomLabel);
+            bottomLabel = new Label(Comms.portName.get());
+            bottomLabel.textProperty().bind(Comms.portName);
+            portLabelBox.getChildren().add(bottomLabel);
+            bottomBox.getChildren().add(portLabelBox);
+            bottomBox.setId("bottomBox");
+            
         getChildren().addAll(menuBar, hbox, bottomBox);
     	}
     }
